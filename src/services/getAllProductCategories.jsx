bb562a6dd77f8894329a39e83115758b34dddb12
JihanSalsabilah14 <jihansalsabilah.22013@mhs.unesa.ac.id>
2024-11-18 15:34:38 +0700

    React Jihan Salsabilah

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,21 @@
+const getAllProductCategories = () => {
+  return [
+    {
+      id: "BS",
+      slug: "badut-sulap",
+      name: `Badut Sulap`,
+    },
+    {
+      id: "BA",
+      slug: "badut-atraksi",
+      name: `Badut Atraksi`,
+    },
+    {
+      id: "BK",
+      slug: "badut-klasik",
+      name: `Badut Klasik`,
+    },
+  ];
+};
+
+export default getAllProductCategories;
